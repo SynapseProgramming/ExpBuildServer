2c0d6774b4e080c55f2386d715e41a4bb1dbb7e7
Roald Ong <roald.ong@gmail.com>
2022-08-10 21:57:31 +0800

    Add in queue example

--- src/main.c
+++ src/main.c
@@ -1,90 +1,64 @@
-/* UART Echo Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/uart.h"
-#include "driver/gpio.h"
-#include "sdkconfig.h"
-#include "esp_log.h"
-#include <string.h>
+#include "freertos/queue.h"
 
-/**
- * This is an example which echos any data it receives on configured UART back to the sender,
- * with hardware flow control turned off. It does not use UART driver event queue.
- *
- * - Port: configured UART
- * - Receive (Rx) buffer: on
- * - Transmit (Tx) buffer: off
- * - Flow control: off
- * - Event queue: off
- * - Pin assignment: see defines below (See Kconfig)
- */
+TaskHandle_t task_send_Handle = NULL;
+TaskHandle_t task_receive_Handle = NULL;
+QueueHandle_t queue;
 
-#define ECHO_TEST_TXD 17
-#define ECHO_TEST_RXD 16
-#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
-#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
-
-#define ECHO_UART_PORT_NUM 2 // second uart. gpio tx: 17 gpio rx: 16
-#define ECHO_UART_BAUD_RATE 115200
-#define ECHO_TASK_STACK_SIZE 2048
-
-static const char *TAG = "UART TEST";
-
-#define BUF_SIZE (1024)
-
-static void echo_task(void *arg)
+void task_send(void *arg)
 {
-    /* Configure parameters of an UART driver,
-     * communication pins and install the driver */
-    uart_config_t uart_config = {
-        .baud_rate = ECHO_UART_BAUD_RATE,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_REF_TICK,
-    };
-    int intr_alloc_flags = 0;
+    char string_send[9];
+    sprintf(string_send, "data 1");
+    xQueueSend(queue, (void *)string_send, pdMS_TO_TICKS(100));
+    printf("Send:  %s \n", string_send);
 
-#if CONFIG_UART_ISR_IN_IRAM
-    intr_alloc_flags = ESP_INTR_FLAG_IRAM;
-#endif
+    sprintf(string_send, "data 2");
+    xQueueSend(queue, (void *)string_send, pdMS_TO_TICKS(100));
+    printf("Send:  %s \n", string_send);
 
-    ESP_ERROR_CHECK(uart_driver_install(ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
-    ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
-    ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
+    sprintf(string_send, "data 3");
+    xQueueSend(queue, (void *)string_send, pdMS_TO_TICKS(100));
+    printf("Send:  %s \n\n", string_send);
 
-    // create an array of 1 element. 
-    size_t array_size = 2 * sizeof(uint8_t);
-    uint8_t *data = (uint8_t *)malloc(array_size);
-    
-    data[0] = 7;
-    data[1] = 3;
-   // char *test_str = "This is a test string.\n";
     while (1)
     {
-        // Read data from the UART
-        // int len = uart_read_bytes(ECHO_UART_PORT_NUM, data, (BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
-        // Write data back to the UART
-
-        // uart_write_bytes(ECHO_UART_PORT_NUM, (const int *)data, sizeof(num));
-        // uart_write_bytes(ECHO_UART_PORT_NUM, (const char *)test_str, strlen(test_str));
-        uart_write_bytes(ECHO_UART_PORT_NUM, (const uint8_t *)data, array_size);
-        ESP_LOGI("test", "working");
-
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
-void app_main(void)
+void task_receive(void *arg)
 {
-    xTaskCreate(echo_task, "uart_echo_task", ECHO_TASK_STACK_SIZE, NULL, 10, NULL);
+    char string_receive[9];
+    while (1)
+    {
+        if (xQueueReceive(queue, &(string_receive), pdMS_TO_TICKS(100)))
+        {
+            printf("Received:  %s \n", string_receive);
+        }
+    }
+}
+
+void app_main()
+{
+    // Queue creation
+    queue = xQueueCreate(5, 10);
+
+    // Available
+    printf("\nAvailable at the begining: %d \n\n", uxQueueSpacesAvailable(queue));
+
+    // Write data to queue
+    xTaskCreate(task_send, "task_send", 4096, NULL, 10, &task_send_Handle);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+
+    // Available
+    printf("Available after writing: %d \n\n", uxQueueSpacesAvailable(queue));
+
+    // Read data from queue
+    xTaskCreate(task_receive, "task_receive", 4096, NULL, 10, &task_receive_Handle);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+
+    // Available
+    printf("\nAvailable at the end: %d \n\n", uxQueueSpacesAvailable(queue));
 }
